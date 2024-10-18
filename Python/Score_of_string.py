d395c42d1e2c417fd92f5217556c9031fe5389f4
ROHITH1709-byte <24bct140@kgcas.com>
2024-10-19 04:21:15 +0530

    Update Score_of_string.py

--- Python/Score_of_string.py
+++ Python/Score_of_string.py
@@ -1,5 +1,12 @@
-def scoreOfString(self, s):
-  res = 0
-  for i in range(len(s)-1):
-    res += abs(ord(s[i]) - ord(s[i+1]))
-  return res
+def scoreOfString(s):
+    if not s:  # Handle empty string case
+        return 0
+    
+    res = 0
+    for i in range(len(s) - 1):
+        res += abs(ord(s[i]) - ord(s[i + 1]))
+    return res
+
+# Example usage
+s = "abc"
+print("Score of the string:", scoreOfString(s))  # Output: 2
