a7bddb325da4c295d43f71733631407b5ed47d45
ROHITH1709-byte <24bct140@kgcas.com>
2024-10-19 04:08:56 +0530

    Update Binary-Search.py

--- Python/Binary-Search.py
+++ Python/Binary-Search.py
@@ -1,20 +1,23 @@
+def binary_search(array, target):
+    left, right = 0, len(array) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if array[mid] == target:
+            return mid  # Return the index where the target is found
+        elif array[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1  # Return -1 if the target is not found
+
+# Example usage
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 target = 7
-left = 0
-right = len(array) - 1
-found = False
+index = binary_search(array, target)
 
-while left <= right:
-    mid = left + (right - left) // 2
-    if array[mid] == target:
-        found = True
-        break
-    elif array[mid] < target:
-        left = mid + 1
-    else:
-        right = mid - 1
-
-if found:
-    print("Element found at index:", mid)
+if index != -1:
+    print("Element found at index:", index)
 else:
     print("Element not found")
